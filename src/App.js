40ce04c0ed6e6d35c44f4c73d3d7f681aed44ca6
Guilherme Motta <guimotta7@gmail.com>
2023-08-31 01:30:31 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {Alert, AlertIcon, Body, Header, Searchbar, Profile, UserIcon, UserInfo, UserRanking, UserLocation, HorizontalRuler, StarIcon, BlueText, ArrowIcon, MoreContent} from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTriangleExclamation, faUser } from '@fortawesome/free-solid-svg-icons'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <Searchbar type="text" placeholder="Buscar capacitações, profissionais..." />
+      </Header>
+
+      <Body>
+        <Alert> 
+          <AlertIcon><FontAwesomeIcon icon={faTriangleExclamation} size="xl"/></AlertIcon>
+          <p><strong>Completar perfil:</strong> Preencha suas informações para encontrar pacientes.</p>
+        </Alert>
+
+        <Profile>
+          <p>Seu perfil</p>
+          <UserInfo>
+            <UserIcon><FontAwesomeIcon icon={faUser} size="2xl"/></UserIcon>
+            <div>
+              <strong>Carolina Magalhães</strong>
+              <UserLocation>São Paulo - SP</UserLocation>
+              <UserRanking>
+                <StarIcon></StarIcon>
+                <StarIcon></StarIcon>
+                <StarIcon></StarIcon>
+                <StarIcon></StarIcon>
+                <StarIcon></StarIcon>
+                (0) 
+              </UserRanking>
+            </div>
+          </UserInfo>
+          
+        </Profile>
+        <HorizontalRuler></HorizontalRuler>
+        
+        <BlueText>
+          Completar perfil<ArrowIcon></ArrowIcon>
+        </BlueText>
+
+        <MoreContent>
+          <p>Capacitações profissionais</p>
+          <BlueText>
+            Ver mais
+          </BlueText>
+          
+        </MoreContent>
+        <Swiper 
+            spaceBetween={50}
+            slidesPerView={1.75}
+          >
+            <SwiperSlide><img src='https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4'></img></SwiperSlide>
+            <SwiperSlide><img src='https://s5.static.brasilescola.uol.com.br/be/2023/01/diversas-frutas-em-fundo-branco.jpg'></img></SwiperSlide>
+          </Swiper>
+      </Body>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,144 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowRight, faStar } from '@fortawesome/free-solid-svg-icons'
+
+export const Header = styled.header
+`
+    padding: 30px 0;
+    width: 100%;
+    background-color: #006557;
+`
+
+export const Searchbar = styled.input
+`
+    font-size: 14px;
+    color: #6D7A78; 
+    padding-left: 30px;
+    width: 80%;
+    height: 35px;
+    border-radius: 7px;
+    border: none;
+    background-image: url('https://www.svgrepo.com/show/64147/magnifying-glass.svg');
+    background-size: 3.5%;
+    background-repeat: no-repeat;
+    background-position: 10px center;
+    font-weight: 500;
+`
+
+export const Body = styled.div
+`
+    padding: 22px;
+`
+
+export const Alert = styled.div
+`
+    color: #8b6421;
+    padding: 0px 20px;
+    font-size: 14px;
+    background-color: #f8f4c4;
+    border-radius: 7px;
+    border: 1px solid #8b6421;
+    display: flex;
+    flex-direction: row;
+    text-align: left;
+    font-weight: 400;
+`
+
+export const AlertIcon = styled.div
+`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+`
+
+export const Profile = styled.div
+`
+    color: #424F4D;
+    text-align: left;
+    font-size: 18px;
+    margin: 30px 0;
+    font-weight: 600;
+`
+
+export const UserIcon = styled.div
+`   
+    background-color: #D3D3D3;
+    color: #5D5D5D;
+    border-radius: 100%;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 15px;
+`
+
+export const UserInfo = styled.div
+`   
+    display: flex;
+    flex-direction: row;
+    font-weight: 200;
+`
+
+export const UserRanking = styled.div
+`   
+    margin-top: 15px;
+    color: #D3D3D3;
+    font-size: 12px;
+    font-weight: 400;
+`
+
+export const UserLocation = styled.p
+`   
+    color: #6D7A78;
+    font-size: 12px;
+    line-height: 0;
+    font-weight: 400;
+`
+
+export const HorizontalRuler = styled.hr
+`   
+    height: 1px;
+    background-color: #D3D3D3;
+    border: none;
+    margin-top: 20px;
+`
+
+const StyledIcon = styled(FontAwesomeIcon)
+`   
+    margin-right: 5px;
+`
+
+export const StarIcon = () => <StyledIcon icon={faStar} size="lg"/>
+
+const StyledIconArrow = styled(FontAwesomeIcon)
+`   
+    margin-left: 12px;
+`
+
+export const ArrowIcon = () => <StyledIconArrow icon={faLongArrowRight} size="lg"/>
+
+export const BlueText = styled.p
+`   
+    font-weight: 600;
+    color: #07689F;
+    font-size: 18px;
+`
+
+export const MoreContent = styled.div
+`
+    color: #424F4D;
+    text-align: left;
+    font-size: 18px;
+    margin: 40px 0;
+    font-weight: 600;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const CarouselCard = styled.div
+`
+    
+`
